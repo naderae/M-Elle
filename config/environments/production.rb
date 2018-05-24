380f1bcac3bd79d5683e0fbc0c9f641a44d76f99
nader abou ezze <naderabouezze93@gmail.com>
2018-05-24 15:38:39 +0300

    testing envs/production.rb swettings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,12 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'mnelle.co' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
 
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   # :domain               => "example.com",
+   # :domain               => "mnelle.co",
    :user_name            => ENV["USERNAME"],
    :password             => ENV["PASSWORD"],
    :authentication       => "plain",
