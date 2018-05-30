224dbb97733ba47bf9d15404c9a5ea76fac03cac
nader abou ezze <naderabouezze93@gmail.com>
2018-05-30 12:31:19 +0300

    fixed region for AWS S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-east-ohio' ,
+      region: 'us-east-2' ,
       path_style: true
     }
     config.storage :fog
